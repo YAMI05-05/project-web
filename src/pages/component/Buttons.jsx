1cdbc78b7958decdccace3407327e46b9e87e011
YAMI05-05 <pandeyrupak5@gmail.com>
2025-06-25 15:48:13 +0545

    Delete src directory

--- src/pages/component/Buttons.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-function Buttons({ label, onClick,  }) {
-  return <button onClick={onClick} className= 'bg-red-500 text-white'>{label}
-  </button>;  }   
-
- export default Buttons;
